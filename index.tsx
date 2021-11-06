6004b8d67ed87e7fe2b5789635dfc951fd80ff0b
Maksym Ferber <maksim.ferber@gmail.com>
2021-11-06 12:55:46 +0200

    ra-tinymce-input (#1)
    * ra-tinymce-input
    
    * Removed unused code and unnecessary deps
    Added missing deps
    
    Co-authored-by: Maks Ferber <maksferber@MacBook-Pro-Maks.local>
    Co-authored-by: Sergey Mell <sergey.mell@agilie.com>

---
+++ index.tsx
@@ -0,0 +1,82 @@
+import { ComponentProps } from 'react';
+import { useInput } from 'ra-core';
+import { InputHelperText } from 'ra-ui-materialui';
+import { PropTypes as MuiPropTypes, } from '@material-ui/core';
+import * as PropTypes from 'prop-types';
+import { Editor } from '@tinymce/tinymce-react';
+
+export interface RichTextInputProps {
+    label?: string | false;
+    source: string;
+    toolbar?: string;
+    fullWidth?: boolean;
+    helperText?: ComponentProps<typeof InputHelperText>['helperText'];
+    record?: Record<any, any>;
+    resource?: string;
+    variant?: string;
+    margin?: MuiPropTypes.Margin;
+    [key: string]: any;
+}
+
+export const RichTextInput = (props: RichTextInputProps) => {
+    const {
+        toolbar = false,
+        menubar = false,
+        fullWidth = true,
+        label,
+        source,
+        plugins,
+        content_style,
+        textareaName,
+        tagName = 'div',
+        scriptLoading,
+        inline = false,
+        initialValue,
+        disabled = false,
+        cloudChannel = '5-stable',
+        apikey = 'c3qq8pm65834hbfehad1ewk7ofgkvm1mmhg41vpruadnn5w3',
+        ...rest
+    } = props;
+    const {
+        id,
+        isRequired,
+        input: {value, onChange},
+        meta: {touched, error},
+    } = useInput({source, ...rest});
+
+    return (
+        <>
+            <Editor
+                value={value}
+                init={{
+                    menubar: menubar,
+                    plugins: plugins,
+                    toolbar: toolbar,
+                    content_style: content_style,
+                }}
+                onInit={(evt, editor) => {
+                }}
+                onEditorChange={(newValue, editor) => {
+                    onChange(newValue)
+                }}
+                apiKey={apikey}
+                cloudChannel={cloudChannel}
+                disabled={disabled}
+                initialValue={initialValue}
+                inline={inline}
+                scriptLoading={scriptLoading}
+                tagName={tagName}
+                textareaName={textareaName}
+            />
+        </>
+    );
+};
+
+RichTextInput.propTypes = {
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+    options: PropTypes.object,
+    source: PropTypes.string,
+    fullWidth: PropTypes.bool,
+    configureQuill: PropTypes.func,
+};
+
