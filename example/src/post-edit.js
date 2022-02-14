18d0f37f699c1a762bfde6bf7047218334587481
Maksym Ferber <maksim.ferber@gmail.com>
2022-02-14 17:36:42 +0200

    mrework props adding (#2)
    Co-authored-by: Maks Ferber <maksferber@MacBook-Pro-Maks.local>

--- example/src/post-edit.js
+++ example/src/post-edit.js
@@ -6,7 +6,22 @@ export const PostEdit = (props) => (
     <Edit {...props}>
         <SimpleForm>
             <TextInput source="title" />
-            <RichTextInput source="body" toolbar={true} />
+            <RichTextInput source="body" 
+            initialValue="<p>This is the initial content of the editor.</p>"
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+              ],
+              toolbar: 'undo redo | formatselect | ' +
+              'bold italic backcolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }} />
             <DateInput label="Publication date" source="published_at" defaultValue={new Date()} />
         </SimpleForm>
     </Edit>
